6c2151c092acfd2c839a64af46201ed5f4cdcad3
sandbox <netmesh@outlook.com>
2024-03-08 15:23:49 +0800

    Initial

---
+++ a4n/setup.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+# prints colored text
+success () {
+    COLOR="92m"; # green
+    STARTCOLOR="\e[$COLOR";
+    ENDCOLOR="\e[0m";
+    printf "$STARTCOLOR%b$ENDCOLOR" "done\n";
+}
+
+echo "Uninstall Ansible"
+printf "Uninstall Ansible"
+sudo pip uninstall ansible==2.9.4 -y
+success
+
+echo "Install Ansible 3.0"
+printf "Install Ansible 3.0"
+pip install ansible==3.0
+success
+
+echo "Install Paramiko 3.0"
+printf "Install Paramiko 3.0"
+pip install paramiko==3.0
+success 
+
+echo "Install ansible-pylibssh"
+printf "ansible-pylibssh"
+pip install ansible-pylibssh
+success
+
+
+echo "Install Python Netaddr"
+printf "Install Python Netaddr"
+pip install netaddr
+success
+
+echo "Git Clone my_project"
+printf "Git Clone my_project"
+cd ~
+git clone https://gitlab.com/${gitlab_user}/my_project.git
+success
+
+echo "Configure GIT"
+printf "Configure GIT"
+cd ~
+git config --global user.name ${gitlab_user}
+git config --global user.email "you@example.com"
+success
+
+echo "Install NXOS Collection"
+printf "Install NXOS Collection"
+ansible-galaxy collection install cisco.nxos
+success
+
+echo "Install IOS Collection"
+printf "Install IOS Collection"
+ansible-galaxy collection install cisco.ios
+success
+
+echo "Verify my_project is present"
+printf "Verify my_project is present"
+cd ~
+tree my_project
+success
